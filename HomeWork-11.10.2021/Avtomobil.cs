b30e0bf093c36d05969ee547538b189dac17dd4d
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-10-11 22:09:17 +0400

    Add project files.

---
+++ HomeWork-11.10.2021/Avtomobil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_11._10._2021
+{
+    class Avtomobil
+    {
+        private int _maxbenzintutumu; //max benzin tutumu =50
+        private double _benzin;
+        private double _kilometr;
+        private double _pul;
+        public int Benzintutumu { get => _maxbenzintutumu; set => _maxbenzintutumu = value; }
+        public double Kilometr { get => _kilometr; set => _kilometr = value; }
+        public double Benzin { get => _benzin; set => _benzin = value; }
+        public double Pul { get => _pul; set => _pul = value; }
+
+        public string YuzKilometreYandirilanBenzin()
+        {
+            //Teqriben 100 km-e 10 litr benzin goturdum
+            _benzin = _kilometr / 10;
+            return $"100 km-ya yandirilan benzin: {_benzin} Litr";
+        }
+
+        public void YanacaqDoldurma(int litr)
+        {
+            _pul = litr * 1.2;
+            _benzin += litr;
+            if (_benzin <= 50)
+            {
+               
+                Console.WriteLine($"Odeyeceyiniz Mebleg: {_pul} manat");
+                Console.WriteLine($"Benzin: {_benzin} Litr");
+
+            }
+            if (_benzin > 50)
+            {
+                Console.WriteLine("Chen maksimum 50 litr tutur.");
+            }
+           
+        }
+        public string TamDoldurmaq()
+        {
+            _pul = Convert.ToInt32((_maxbenzintutumu - _benzin) * 1.2); //1 litr 1.20 man hesabladim
+            _benzin = 50;
+            Console.WriteLine($"Odeyeceyiniz mebleg: {_pul} manat");
+            return $"Benzin: {_benzin} Litr";
+        }
+    }
+}
---
+++ HomeWork-11.10.2021/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeWork_11._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avtomobil avt = new Avtomobil();
+
+            avt.Benzintutumu = 50;
+            Console.WriteLine($"Maksimum benzin tutumu: {avt.Benzintutumu} Litr \n");
+
+            avt.Benzin = 50;
+            Console.WriteLine($"Hal-hazirda olan benzin: {avt.Benzin} Litr \n");
+
+            avt.Kilometr = 345;
+            Console.WriteLine($"Gedilen Yol: {avt.Kilometr} Km \n");
+
+            //Console.WriteLine(avt.YuzKilometreYandirilanBenzin() + "\n");
+
+            avt.YanacaqDoldurma(5);
+
+            //Console.WriteLine(avt.TamDoldurmaq());
+
+        }
+    }
+}
